d4c65cb53cf00951d93f6c56e54c46b0095de92c
Jesimiel Nóbrega <jesimielnobrega25@gmail.com>
2024-07-15 13:45:00 +0100

    feat: :sparkles:

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -17,12 +17,19 @@ export async function createTrip(app: FastifyInstance) {
           ends_at: z.coerce.date(),
           owner_name: z.string(),
           owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
         }),
       },
     },
     async (request) => {
-      const { destination, ends_at, starts_at, owner_email, owner_name } =
-        request.body;
+      const {
+        destination,
+        ends_at,
+        starts_at,
+        owner_email,
+        owner_name,
+        emails_to_invite,
+      } = request.body;
 
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error("Ivalid trip start date.");
@@ -54,6 +61,21 @@ export async function createTrip(app: FastifyInstance) {
           destination,
           ends_at,
           starts_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_confirmed: true,
+                  is_owner: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email };
+                }),
+              ],
+            },
+          },
         },
       });
 
